65b1c0465bb5019005bc5a0b461ec23d8798eb3c
NateM926 <natem926rr@gmail.com>
2019-05-03 00:54:33 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <AppMap/>
+  </div>
+</template>
+
+<script>
+import AppMap from "./components/AppMap";
+
+export default {
+  name: "App",
+  components: {
+    AppMap
+  }
+};
+</script>
+
+<style>
+</style>
